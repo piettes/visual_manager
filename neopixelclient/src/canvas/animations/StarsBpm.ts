eb7d8a2b05c18e1c108363192fcb7adc43e75c2f
Stéphane Piette <stephane.piette@codecentric.de>
2017-10-21 19:11:56 +0200

    test

--- neopixelclient/src/canvas/animations/StarsBpm.ts
+++ neopixelclient/src/canvas/animations/StarsBpm.ts
@@ -129,9 +129,10 @@ class StarsBpm extends AnimationBase implements Animation {
       this.timeAcc -= this.animDuration;
       return 0;
     }
-    let caca =  Math.floor(this.animDuration / (this.animDuration - this.timeAcc) * this.limit) + 1;
+    let caca =  Math.floor(this.timeAcc / this.animDuration * this.limit) + 1;
     console.log("this.animDuration", this.animDuration);
     console.log("this.timeAcc", this.timeAcc);
+    console.log("tcaca", caca);
     return caca;
     //
     // if (this.ticker >= 60 * AnimationBase.FPS_RATE / this.bpm) {
