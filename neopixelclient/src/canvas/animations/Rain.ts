436ea3dd8a8a5420448d73b0218c190c7b5655fc
Stéphane Piette <stephane.piette@codecentric.de>
2017-10-25 23:10:46 +0200

    test rain

--- neopixelclient/src/canvas/animations/Rain.ts
+++ neopixelclient/src/canvas/animations/Rain.ts
@@ -5,47 +5,46 @@ import Color from "../Color";
 
 class Rain extends AnimationBase implements Animation {
 
-  offsets: Array<any> = [];
+  // offsets: Array<any> = [];
 
   constructor() {
     super();
-    this.initOffsets();
+    // this.initOffsets();
   }
 
   getName(): string {
     return "Rain";
   }
 
-  initOffsets() {
-    let offsets = [];
-    offsets.push(0);
-    offsets.push(Math.floor(Math.random() * 30) + 10);
-    offsets.push(Math.floor(Math.random() * 30) + 10);
-    offsets.push(Math.floor(Math.random() * 30) + 30);
-    offsets.push(Math.floor(Math.random() * 30) + 30);
-    // https://stackoverflow.com/a/18650169
-    offsets.sort(function () {
-      return .5 - Math.random();
-    });
-    this.offsets = offsets;
-  }
+  // initOffsets() {
+  //   let offsets = [];
+  //   offsets.push(0);
+  //   offsets.push(Math.floor(Math.random() * 30) + 10);
+  //   offsets.push(Math.floor(Math.random() * 30) + 10);
+  //   offsets.push(Math.floor(Math.random() * 30) + 30);
+  //   offsets.push(Math.floor(Math.random() * 30) + 30);
+  //   // https://stackoverflow.com/a/18650169
+  //   offsets.sort(function () {
+  //     return .5 - Math.random();
+  //   });
+  //   this.offsets = offsets;
+  // }
 
   nextframe(frame: Array<Point>, tick: number): boolean {
-    this.array.forEach(y => {
-      if (tick === this.offsets[y]) {
-        let color: Color = Math.random() > 0.5 ? this.color1 : this.color2;
-        let shade: number = color.getShade(Math.floor(Math.random() * 6));
-        let position: number = Math.floor(Math.random() * this.numLED);
-        frame.push({x: position, y: y, c: shade});
-      }
-    });
+    if (tick === 0) {
+      let color: Color = Math.random() > 0.5 ? this.color1 : this.color2;
+      let shade: number = color.getShade(Math.floor(Math.random() * 6));
+      let x: number = Math.floor(Math.random() * this.numLED);
+      let y: number = Math.floor(Math.random() * this.array.length);
+      frame.push({x: x, y: y, c: shade});
+    }
     return true;
   }
 
   tick(timeDiff: number): number {
     if (this.ticker >= 80) {
       this.ticker = 0;
-      this.initOffsets();
+      // this.initOffsets();
       return this.ticker;
     }
     this.ticker++;
