0a44ac0ccf68a0063acef5e42c4fbf916b9de190
Stéphane Piette <stephane.piette@codecentric.de>
2019-10-27 14:06:08 +0100

    test

--- neopixelclient/src/canvas/animations/FadeInOutSmall2Half_.ts
+++ neopixelclient/src/canvas/animations/FadeInOutSmall2Half_.ts
@@ -29,8 +29,10 @@ class FadeInOutSmall2Half extends AnimationBase implements Animation {
     }
     console.log(this.timeAcc)
     if (this.timeAcc < this.animDuration / 4) {
+      console.log("this.timeAcc < this.animDuration / 4 " +  Math.floor(this.timeAcc * 32 / this.animDuration));
       return Math.floor(this.timeAcc * 32 / this.animDuration);
     } else if (this.timeAcc < this.animDuration / 2) {
+      console.log("this.timeAcc < this.animDuration / 2 " + Math.floor(32 - (this.timeAcc * 32 / this.animDuration)));
       return Math.floor(32 - (this.timeAcc * 32 / this.animDuration));
     } else {
       return 0;
