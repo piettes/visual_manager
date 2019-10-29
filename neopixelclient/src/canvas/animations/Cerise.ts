724ba04daca56f5ca0d3c29135f021d2526e51ad
Stéphane Piette <stephane.piette@codecentric.de>
2019-10-29 22:39:17 +0100

    test

--- neopixelclient/src/canvas/animations/Cerise.ts
+++ neopixelclient/src/canvas/animations/Cerise.ts
@@ -10,6 +10,7 @@ class Cerise extends AnimationBase implements Animation {
 
   nextframe(frame: Array<number>, tick: number): boolean {
 
+    if (tick === 1) {
     frame[0] = Color.RED.value;
     frame[1] = Color.RED.value;
     frame[2] = Color.DEEP_PINK.value;
@@ -18,14 +19,39 @@ class Cerise extends AnimationBase implements Animation {
     frame[5] = Color.CYAN.value;
     frame[6] = 0x1256f9;
     frame[7] = Color.DODGER_BLUE.value;
-
+    }
+    if (tick === 2) {
+      frame[0] = Color.PURPLE.value;
+      frame[20] = Color.DODGER_BLUE.value;
+      frame[21] = Color.GREEN.value;
+      frame[22] = Color.CHARTREUSE.value;
+      frame[23] = Color.ORANGE.value;
+      frame[24] = 0x3edde6;
+      frame[30] = 0x3edde6;
+      frame[31] = 0x3edde6;
+      frame[32] = 0x3edde6;
+    }
     return true;
   }
 
   tick(timeDiff: number): number {
-    console.log(timeDiff)
-    return 1;
+    this.timeAcc += timeDiff;
+
+
+    let vanille = 1;
+
+    if (this.timeAcc > 500) {
+      vanille = 2;
+    }
+
+    if ( this.timeAcc > 1000) {
+      this.timeAcc = 0;
+    }
+
+    return vanille;
   }
+
+
 
 }
 
